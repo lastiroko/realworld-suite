1ba15a32212323455529fb3f6f8277560d518b9c
Philip Olaomo <philipoluwalani@gmail.com>
2025-10-11 05:15:37 +0200

    feat(api): add hello endpoint, CORS config, initial migration

---
+++ apps/dsar-workflow/src/test/java/com/realworld/dsar/DsarWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realworld.dsar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsarWorkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
